ab61a5bb666437a1a14313d76807adc733098bd2
vishnu389 <vishnuvardhan96812@gmail.com>
2022-10-04 12:01:00 +0530

    first release of hotel app

---
+++ src/test/MyFirstAutomationScript.java
@@ -0,0 +1,19 @@
+package test;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class MyFirstAutomationScript {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        System.setProperty("webdriver.chrome.driver","C:\\Users\\VPENIGIL\\Desktop\\selinium\\chromedriver_win32\\chromedriver.exe");
+        WebDriver driver=new ChromeDriver();
+        driver.get("https://adactinhotelapp.com");
+        driver.findElement(By.id("username")).sendKeys("vishnu96812");
+        driver.findElement(By.id("password")).sendKeys("2XV76W");
+        driver.findElement(By.id("login")).click();
+	}
+
+}
